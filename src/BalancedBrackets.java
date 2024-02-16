1a17074d24258109d88bd4ab69900d4203d8c4c1
haileycummings333 <haileycummings333@gmail.com>
2024-02-16 07:58:34 -0600

    initial commit 2/16

---
+++ src/BalancedBrackets.java
@@ -0,0 +1,33 @@
+public class BalancedBrackets {
+    /*
+    A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a
+closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {},
+and ().
+A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For
+example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square
+brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single,
+unbalanced closing square bracket, ].
+By this logic, we say a sequence of brackets is balanced if the following conditions are met:
+• It contains no unmatched brackets
+• The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched
+pair of brackets
+Given a String of brackets, determine whether the sequence of brackets is balanced. If a String is balanced,
+return YES. Otherwise, return NO.
+Function Description: Write a function named isBalanced.
+isBalanced has the following parameter(s):
+• String s: a String of brackets
+Returns
+• String: either YES or NO
+Input Format
+A single String s will contain only a sequence of brackets. Can be empty.
+Constraints
+• 1 ≤ |𝑠| ≤ 10! , where |𝒔| is the length of the sequence
+• All characters in the sequences ∈ { ‘{‘, ‘}’, ‘(‘, ‘)’, ‘[‘, ‘]’ }
+• You are allowed to use the LinkedList class but only the add(), remove(), and peek() methods
+Output Format
+For each String, return YES or NO
+     */
+
+
+}
---
+++ src/Postfix.java
@@ -0,0 +1,23 @@
+public class Postfix {
+    /*
+    Given a String input containing an infix expression. Return a String output containing the equivalent
+postfix expression.
+Infix expression: The expression of the form a op b. When an operator is in-between every pair of
+operands.
+Postfix expression: The expression of the form a b op. When an operator is followed for every pair of
+operands.
+Note: The order of precedence is: ^ greater than * equals to / greater than + equals to -. Ignore the right
+associativity of ^.
+Hint:
+o If the scanned character is a ( , push it to the stack.
+o If the scanned character is a ) , pop the stack and output it until a ( is encountered, and discard both
+the parenthesis.
+Constraints
+• The input is guaranteed to be a valid expression
+ample Input
+a+b*(c^d-e)^(f+g*h)-i
+Sample Output
+abcd^e-fgh*+^*+i-
+     */
+
+}
