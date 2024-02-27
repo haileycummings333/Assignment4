5afb5ff22f9f4513889e3c25858f77ed73080bd0
haileycummings333 <haileycummings333@gmail.com>
2024-02-27 13:17:37 -0600

    dna to rna prob. 2/27

--- src/DNARNA.java
+++ src/DNARNA.java
@@ -13,6 +13,46 @@ AGCTGGGAAACGTAGGCCTA
 Sample Output
 AGCUGGGAAACGUAGGCCUA
      */
+    public static String transcribeToRna(String dnaSequence) {
+        StringBuilder rnaSequence = new StringBuilder();
 
+        for (char nucleotide : dnaSequence.toCharArray()) {
+            switch (nucleotide) {
+                case 'A':
+                    //rnaSequence+='A';
+                    rnaSequence.append('A');
+                    break;
+                case 'C':
+                    //rnaSequence+='C';
+                    rnaSequence.append('C');
+                    break;
+                case 'G':
+                    //rnaSequence+='G';
+                    rnaSequence.append('G');
+                    break;
+                case 'T':
+                    //rnaSequence+='U';
+                    rnaSequence.append('U');
+                    break;
+                default:
+                    // if characters not allowed
+                    break;
+            }
+        }
+
+        return rnaSequence.toString();
+    }
+
+    public static void main(String[] args) {
+        // examples
+        String dna1 = "AGCTGGGAAACGTAGGCCTA";
+        String dna2 = "TTTTTTTTTTGGCGCG";
+        String dna3 = "CTTTGGGACTAGTAACCCATTTCGGCT";
+
+        //print the result
+        System.out.println(transcribeToRna(dna1));  // output: AGCUGGGAAACGUAGGCCUA
+        System.out.println(transcribeToRna(dna2));  // output: UUUUUUUUUUGGCGCG
+        System.out.println(transcribeToRna(dna3));  // output: CUUUGGGACUAGUAACCCAUUUCGGCU
+    }
 
 }
